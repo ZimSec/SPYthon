5652962eec394fa68f1f920f5056f3ea4ba66764
ZJam <32304425+ZimSec@users.noreply.github.com>
2021-03-10 18:03:32 -0500

    Updated spython.py: Add lots of new stuff
    Added:
    - Nmap scanning
    - Status log support
    - Ascii Art
    - Support for receiving client data

--- spython.py
+++ spython.py
@@ -2,19 +2,93 @@ import socket                                                           # Import
 from threading import Thread                                            # Import Threading
 from socketserver import ThreadingMixIn                                 # Import socketserver from threadingmixin
 import pyinputplus as pp                                                # Import pyinputplus for validation
+import datetime                                                         # Imports date time for status updates
+import nmap3                                                            # This is for scannign through nmap
+import pprint                                                           # This is for incorporating formatted dictionaries
+
+# SPYython Ascii Art
+asciiart=b"""
+                                          .,cdk0XNNNK0xo:.
+                                       .;d0WMMNK0OkO0XWMWNOl'
+                                     .c0WMWKdc,. .;od:;lkXWMXx;
+                                   .c0WMNx;.      .,k0o. .cOWMNk,
+                                  ,kWMNx,           .,k0o. .:0WMXo.
+                                 cXMW0;          .;ox,.,x0o. .oXMWO,
+                               .dNMNx.             .o0x,.,k0o. ;0WMK:
+                              .kWMXl.             ....o0x,.,x0o''kWMXl
+                             .OWMX:              .,okc..o0x,.,x0o;xNMNo.
+                            .OWMK;                 .:kOc..o0x;.,x0k0WMNo.
+                           .OWMK;                 .'..:kOc..o0x;.,xXWMMNo
+                          .kWMK:                 .,o0d'.:kOc..o0k;.,xNMMNl             \033[40m\033[1;34mWelcome to SPYthon!\033[40m\033[0m
+                         .xWMX:                 .. .:OKd'.:kOc..oOk;.,OWMXc
+                        .oWMNl  ..';:clodxkkOO0000000KWMXOxONW0o:lOXk;'kWMK;
+                        lNMW0ook0XNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNWMNkxNMM0'
+                       :XMMMMMMMMMMMWXKKKKKKKKKKKKKKKKKKKKKKKKKNMMMMMMMMMMMWk.
+                      ,0MMMMMMMMMMMMx'.........................:KMMMMMMMMMMMWd.
+                     .xMMMMMMMMMMMMMk.           .:;.          :XMMMMMMMMMMMMN:
+                     .dWMMMMMMMMMMMMNc           .;xd.        .kMMMMMMMMMMMMMX;
+                      .xNMMMMMMMMMMMM0'          ..lx,        lNMMMMMMMMMMMMXc
+                        :0WMMMMMMMMMMWx.                     ;KMMMMMMMMMMMNx'
+                         .c0WMMMMMMMMMWo.                   '0MMMMMMMMMMNx,
+                          .oXMMMMMMMMMMNx'                .:0MMMMMMMMMMM0;
+                       .;dKWMMMMMMMMMMMMMNOl;..       .':dKWMMMMMMMMMMMMMNOl,
+                    .;dKWMMMMMMMMMMMMMMMMMMMWX0kdoooxOKNMMMMMMMMMMMMMMMMMMMMNOl'
+                  ;dKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNOl'
+                ,ONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXd.
+               cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0,
+              cNMMMMMXdlllllccllllllllllllllcclllllllllllllllllllllllllllcclllkWMMMMMO'
+             ;KMMMMMMx.                                                       ;KMMMMMWx.
+            'OMMMMMMMk.                                                       ;XMMMMMMWo
+           .xWMMMMMMMk.                       \033[40m\033[1;31m{}{}{}{}\033[40m\033[0m                        :NMMMMMMMXc
+           oWMMMMMMMMO.                    \033[40m\033[1;31m{}{}{}{}{}{}{}\033[40m\033[0m                     :NMMMMMMMMK,
+          :XMMMMMMMMM0'                  \033[40m\033[1;31m{}{}{}{}{}{}{}{}{}\033[40m\033[0m                   cWMMMMMMMMMO.
+         ,KMMMMMMMMMMK,                 \033[40m\033[1;31m{}{}{}{}{}{}{}{}{}{}\033[40m\033[0m                  lWMMMMMMMMMWx.
+        .OMMMMMMMMMMMK,                \033[40m\033[1;31m{}{}{}{}{}{}{}{}{}{}{}\033[40m\033[0m                 oMMMMMMMMMMMNl
+        oWMMMMMMMMMMMX;                \033[40m\033[1;31m{}{}{}{}{}{}{}{}{}{}{}\033[40m\033[0m                 dMMMMMMMMMMMMK;
+       '0MMMMMMMMMMMMX;                \033[40m\033[1;31m{}       {}{}       {}\033[40m\033[0m                 dMMMMMMMMMMMMMo
+       ,KMMMMMMMMMMMMN:                 \033[40m\033[1;31m{}     {}{}{}     {}\033[40m\033[0m                 .xMMMMMMMMMMMMMx.
+       .OMMMMMMMMMMMMWc                  \033[40m\033[1;31m{}{}{}{}{}{}{}{}{}\033[40m\033[0m                  .kMMMMMMMMMMMMWo
+        cNMMMMMMMMMMMWl                  \033[40m\033[1;31m{}{}{}/ || \{}{}{}\033[40m\033[0m                  .kMMMMMMMMMMMM0'
+         lNMMMMMMMMMMMo                  \033[40m\033[1;31m{}{}{}{}{}{}{}{}{}\033[40m\033[0m                  .OMMMMMMMMMMM0,
+          ;0WMMMMMMMMMo                      \033[40m\033[1;31m{}{}{}{}{}\033[40m\033[0m                      '0MMMMMMMMMNx.
+           .oXMMMMMMMMd                       \033[40m\033[1;31m{}{}{}{}\033[40m\033[0m                       '0MMMMMMMW0:.
+            OXMMMMMMMMx.                                                     ,KMMMMMMMW
+              NNNWMMMMO;.....................................................lXMMMMW/`
+                ':OWMMWNNNNNNNNNNXNXXXNXXXNNNNXNNNXXNNNNNNNNNNNNNNNNNNNNNNNXNNMMMNx;
+                  .OMMN0kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkKWMWo
+                  .xMMKl,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,xWMN:
+                   dMMMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMX;
+                   ;KWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNx.     
+"""
 
 class ClientThread(Thread):                                             # Create Client
     def __init__(self,ip,port):                                         # Create a function within the class to set port and IP list new clients
         Thread.__init__(self)
         self.ip = ip                                                    # Store client IP
         self.port = port                                                # Store client port
-        print("[+] New socket started for " + ip + ":" + str(port))     # List connect client with IP & Port
-
+        # Prints new socket info with date time and client IP's
+        print(datetime.datetime.now(),"\033[40m\033[1;32m[+] New socket started for " + ip + ":" + str(port),"\033[40m\033[0m")
     def run(self):                                                      # What is sent to or recived from the client
         while True:                                                     # While loop to make the session until close
-            s.send(b"Hello!\n")                                         # Send Basic Message
+            s.send(asciiart)                                            # Prints Ascii Art
+            # Sends data asking for the client to give an IP to scan
+            s.send(bytes("\n Hello "+ip+"!\n"+"What would you like to scan?: ", 'utf-8'))
+            result=s.recv(SOCKET_BUFFER_SIZE)                           # Recieves user's data and stores it
+            out = str(result.decode('utf-8').rstrip("\n"))              # Formats user's sent data
+            # Prints status of scan starting with datetime
+            print(datetime.datetime.now(),"\033[40m\033[1;33m[*] Scan of",out,"started for client " + ip + ":" + str(port),"\033[40m\033[0m")
+            nm = nmap3.Nmap()                                           # Sets nm = to the nmap function
+            s.send(bytes(str(nm.scan_top_ports(out))+"\n", 'utf-16'))   # Returns scan data to user
+            # Prints status of finished scan
+            print(datetime.datetime.now(),"\033[40m\033[1;33m[+] Scan of",out," finished and to client " + ip + ":" + str(port),"\033[40m\033[0m")
             s.close()                                                   # Close connection
+            # Print the status of the closed connection
+            print(datetime.datetime.now(),"\033[40m\033[1;31m[-] client " + ip + ":" + str(port), "left the server","\033[40m\033[0m")
             break                                                       # Break the loop
+
+# TCP_IP = "0.0.0.0"                                                    # Hardcoded IP to host server on all nics
+# TCP_PORT = 1337                                                       # Harded port 1337
+SOCKET_BUFFER_SIZE = 4096                                               # Set socket buffer size
 
 TCP_IP = pp.inputIP("Enter the server IP address: ")                    # Obtain and sanitize for the IP
 TCP_PORT = pp.inputNum(prompt='Enter server port: ', min=1, max=65353)  # Obtain and sanitize for port
@@ -24,9 +98,11 @@ tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)         # Set So
 tcpServer.bind((TCP_IP, TCP_PORT))                                      # Bind the server port and IP
 threads = []                                                            # Create a list of threads
 
+# Print Status of the connection with start datetime and server IP and Port Info
+print("\n"+str(datetime.datetime.now()),"\033[40m\033[1;34m[*]Waiting for a connection to",TCP_IP,"at",TCP_PORT,"...\033[40m\033[0m")
+
 while True:                                                             # Created an infinite while loop
-    tcpServer.listen(4)                                                 # Start server Listener
-    print("Waiting for a connection to",TCP_IP,"at",TCP_PORT,"...")     # Output server status with IP and Port
+    tcpServer.listen(4)                                                 # Start server Listener     # Output server status with IP and Port
     (s, (ip,port)) = tcpServer.accept()                                 # Accept a new client
     newthread = ClientThread(ip,port)                                   # Create a new thread
     newthread.start()                                                   # Start new thread
